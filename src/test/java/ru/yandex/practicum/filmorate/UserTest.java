a5e1f7a7351012314261c07d5342ff50528c454f
Memfis <Memfis-745@yandex.ru>
2024-09-23 13:22:11 +0300

    ТЗ-12-2 - 1

--- src/test/java/ru/yandex/practicum/filmorate/UserTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserTest.java
@@ -17,7 +17,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.time.LocalDate;
 import java.util.Set;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
